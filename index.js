bfbd933ab427969e4dfd004b4612d27a37dae93a
ApolloGeneral <kensuzuki0225@gmail.com>
2022-01-09 15:21:08 +0800

    accessToken for user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors= require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt= require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT || 5000 ;
 
@@ -60,6 +61,17 @@ async function run() {
             }
             const result= await bookingCollection.insertOne(booking)
             res.send(result)
+        })
+
+        app.get('/jwt',async(req, res)=>{
+            const email= req.query.email;
+            const query={email:email}
+            const user= await usersCollection.findOne(query)
+            if(user){
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return({accessToken: token})
+            }
+            res.status(403).send({accessToken: ''})
         })
         app.post('/users', async(req, res)=>{
             const user= req.body;
