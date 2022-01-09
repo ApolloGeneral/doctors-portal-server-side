dcab1703d303a8648df9abcdabcf8b5f6848f8c7
ApolloGeneral <kensuzuki0225@gmail.com>
2022-01-09 09:24:56 +0800

    booked date and slot get done

--- index.js
+++ index.js
@@ -18,11 +18,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const appointmentOptionsCollection = client.db("doctorsPortal").collection("appointmentOptions")
+        const bookingCollection = client.db("doctorsPortal").collection("bookings")
         
         app.get('/appointmentOptions',async (req, res)=>{
+            const date = req.query.date;
+            console.log(date);
             const query = {};
             const options = await appointmentOptionsCollection.find(query).toArray();
+            const bookingQuery={appointmentDate: date}
+            const alreadyBooked= await bookingCollection.find(bookingQuery).toArray();
+            options.forEach(option=>{
+                const optionBooked= alreadyBooked.filter(book=> book.treatment=== option.name  )
+                const bookedSlots= optionBooked.map(book=> book.slot )
+                console.log(date , option.name, bookedSlots)
+            })
             res.send(options)
+        })
+
+        app.post('/bookings', async(req, res)=>{
+            const booking= req.body
+            // console.log(booking)
+            const result= await bookingCollection.insertOne(booking)
+            res.send(result)
         })
     } finally {
         
