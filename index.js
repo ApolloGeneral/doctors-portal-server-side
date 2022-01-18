5307e107125f6ad8784e98789363f91bba287153
ApolloGeneral <kensuzuki0225@gmail.com>
2022-01-18 14:46:26 +0800

    client intent added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors= require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt= require('jsonwebtoken')
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+
 const port = process.env.PORT || 5000 ;
 
 const app= express()
@@ -103,6 +106,25 @@ async function run() {
             }
             const result= await bookingCollection.insertOne(booking)
             res.send(result)
+        })
+
+
+        app.post('/create-payment-intent', async(req,res)=>{
+            const booking= req.body;
+            const price= booking.price;
+            const amount= price*100;
+
+            const paymentIntent= await paymentIntents.create({
+                currency:'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ],
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+
         })
 
         app.get('/jwt',async(req, res)=>{
